59733834b93b1db345fe54700abc6adadd6aec1b
Du Song <freewizard@gmail.com>
2011-07-24 20:18:14 +0800

    media key; launch options; pref window

---
+++ SPInvocationGrabbing/NSObject+SPInvocationGrabbing.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+SPInvocationGrabbing.h
+//  FanRadio
+//
+//  Created by Du Song on 11-7-24.
+//  Copyright 2011年 竹竿胡同13号2单元703. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject_SPInvocationGrabbing : NSObject
+
+@end
---
+++ SPInvocationGrabbing/NSObject+SPInvocationGrabbing.m
@@ -0,0 +1,23 @@
+//
+//  NSObject+SPInvocationGrabbing.m
+//  FanRadio
+//
+//  Created by Du Song on 11-7-24.
+//  Copyright 2011年 竹竿胡同13号2单元703. All rights reserved.
+//
+
+#import "NSObject+SPInvocationGrabbing.h"
+
+@implementation NSObject_SPInvocationGrabbing
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
